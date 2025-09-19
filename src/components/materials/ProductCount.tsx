98e4e5708ff7d1ed5b90fb893d70e2db03208454
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-09-19 00:13:17 -0700

    better style

--- src/components/materials/ProductCount.tsx
+++ src/components/materials/ProductCount.tsx
@@ -1,34 +1,48 @@
-'use client'
-import { useState } from 'react'
-
+"use client";
+import { useState } from "react";
+import "./materials.css";
 
 interface MyComponentProps {
-  inventoryCount: number
+  inventoryCount: number;
 }
 
 export default function ProductCount({ inventoryCount }: MyComponentProps) {
-    const [selectedCount, setSelectedCount] = useState(0)
+  const [selectedCount, setSelectedCount] = useState(0);
 
-    const handleMinusClick = () => {
-        setSelectedCount(Math.max(0, selectedCount - 1))
-    }
+  const handleMinusClick = () => {
+    setSelectedCount(Math.max(0, selectedCount - 1));
+  };
 
-    const handlePlusClick = () => {
-        setSelectedCount(selectedCount + 1)
-    }
+  const handlePlusClick = () => {
+    setSelectedCount(selectedCount + 1);
+  };
 
-    return (
-    <div>
-        <div>
-            <button onClick={handleMinusClick}>-</button>
-        </div>
-        <div>
-            <p>{selectedCount}</p>
-            <p>{inventoryCount}</p>
-        </div>
-        <div>
-            <button onClick={handlePlusClick}>+</button>
-        </div>
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <button className="count_button left_button" onClick={handleMinusClick}>
+        -
+      </button>
+      <div
+        className={`center_text ${
+          selectedCount > inventoryCount
+            ? "center_text_invalid"
+            : "center_text_valid"
+        }`}
+      >
+        <p className="center_text_count">{selectedCount}</p>
+        <p
+          className={`center_text_inventory ${
+            selectedCount > inventoryCount
+              ? "center_text_inventory_invalid"
+              : ""
+          }`}
+        >
+          {inventoryCount} PCS
+        </p>
+      </div>
+      <button className="count_button right_button" onClick={handlePlusClick}>
+        +
+      </button>
     </div>
-    )
+  );
 }
