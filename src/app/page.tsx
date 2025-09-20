b430ee327558cf0d1c5e24bb0bced6fda3c86f3d
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-09-19 17:46:54 -0700

    home page begins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import { Logo } from "@/components/icons/Icons";
 
 export default function Home() {
   return (
-    <div>
-  
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="w-[500px] h-[500px] flex flex-col justify-center items-center mb-10 bg-white rounded-lg shadow-lg">
+        <div className="icon_div flex flex-row gap-2 items-center">
+          <Logo />
+          <h1 className="icon_text">Tally</h1>
+        </div>
+        <button>Log In with Google</button>
+      </div>
+
+      <footer>
+        <h1>F25 Lava Dev Challenge</h1>
+        <p>By Karen Mkrtchyan</p>
+      </footer>
     </div>
   );
 }
